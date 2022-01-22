1bc8c88f62a396ec55133bd91650ba4e13cdd27b
ZeljkoBenovic <sakal87@gmail.com>
2022-01-22 20:15:15 +0100

    added check for RegisterHandler errors

--- handlers/register_handlers.go
+++ handlers/register_handlers.go
@@ -1,23 +1,33 @@
 package handlers
 
-import "github.com/ivahaev/amigo"
+import (
+	"fmt"
+
+	"github.com/ivahaev/amigo"
+)
 
 func (call *Calls) RegisterHandlers(amigo *amigo.Amigo) {
-	
+	errors := make(map[string]error)
 	// first poing of entry is new channel event
-	amigo.RegisterHandler("Newchannel", call.newChannelHandler)
-	amigo.RegisterHandler("Newstate",call.newStateEventHandler)
-	amigo.RegisterHandler("Hangup",call.hangupEventHandler)
+	errors["new_channel"] = amigo.RegisterHandler("Newchannel", call.newChannelHandler)
+	errors["new_state"] = amigo.RegisterHandler("Newstate",call.newStateEventHandler)
+	errors["hangup"] = amigo.RegisterHandler("Hangup",call.hangupEventHandler)
 
 
 	// // Registering default handler function for all events.
 	// amigo.RegisterDefaultHandler(call.defaultHandler)
 
 	// queue handlers
-	amigo.RegisterHandler("Join", call.queueJoinEventHandler)
-	amigo.RegisterHandler("AgentConnect",call.agentConnectEventHandler)
-	amigo.RegisterHandler("AgentComplete",call.agentCompleteEventHandler)
-	amigo.RegisterHandler("QueueCallerAbandon",call.queueCallerAbandonEventHandler)
+	errors["join"] = amigo.RegisterHandler("Join", call.queueJoinEventHandler)
+	errors["agent_connect"] = amigo.RegisterHandler("AgentConnect",call.agentConnectEventHandler)
+	errors["agent_complete"] = amigo.RegisterHandler("AgentComplete",call.agentCompleteEventHandler)
+	errors["caller_abandon"] = amigo.RegisterHandler("QueueCallerAbandon",call.queueCallerAbandonEventHandler)
+
+	for i, v := range errors {
+		if v != nil {
+			fmt.Println("Could not register handler: ", i, "Error: ",v.Error() )
+		}
+	}
 
 
 
